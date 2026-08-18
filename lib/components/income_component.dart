5fb4e63396449597b05fec2af0255961773e1278
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-08-19 01:43:36 +0200

    Accrue passive income every update tick instead of once per second

--- lib/components/income_component.dart
+++ lib/components/income_component.dart
@@ -1,12 +1,9 @@
 import 'package:fifflar_uffe/game/fifflar_uffe_game.dart';
 import 'package:flame/components.dart';
 
-class IncomeComponent extends TimerComponent
-    with HasGameReference<FifflarUffeGame> {
-  IncomeComponent() : super(period: 1, repeat: true);
-
+class IncomeComponent extends Component with HasGameReference<FifflarUffeGame> {
   @override
-  void onTick() {
-    game.economy.tick(1);
+  void update(double dt) {
+    game.economy.tick(dt);
   }
 }
