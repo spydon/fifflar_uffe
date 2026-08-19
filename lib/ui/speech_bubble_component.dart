f75456fd6cb276fe3056a381876cdfbbd46abff9
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-08-19 10:39:52 +0200

    Make the speech bubble slightly transparent

--- lib/ui/speech_bubble_component.dart
+++ lib/ui/speech_bubble_component.dart
@@ -28,7 +28,7 @@ class SpeechBubbleComponent extends PositionComponent
       SpriteComponent(
         sprite: Sprite(game.images.fromCache(AssetPaths.speechBubble)),
         size: size,
-      ),
+      )..opacity = 0.85,
       _text = TextBoxComponent(
         textRenderer: TextStyles.bubble,
         boxConfig: const TextBoxConfig(maxWidth: 190),
